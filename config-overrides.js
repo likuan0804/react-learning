7d7ab39d1e290eb3b879e0731f468f3d7ede67b2
likuan <kuan.li@chiefclouds.com>
2022-04-18 12:53:23 +0800

    fix

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+module.exports = (config) => {
+  // 单独配置装饰器
+  // 如果没有使用customize-cra,就  在这里可以对config进行配置
+  return config
+}
+
+//自定义的react-config
+// const { override, addDecoratorsLegacy } = require ('customize-cra')//自定义cra,
+
+// module.exports = override(
+//   addDecoratorsLegacy()
+// )
